5aee133213bbb9712e4535b632812e52647323cc
alejandra <dubrasskabreu@gmail.com>
2017-11-11 20:50:19 -0300

    Versión 0.0.2

--- js/app.js
+++ js/app.js
@@ -1,11 +1,34 @@
 function add() {
-	var comments = document.getElementById("msj").value;
+var comments = document.getElementById("msj").value;
 	document.getElementById("msj").value ="";
-	var msj = document.createElement("div");
-	var cont = document.getElementById("cont");
+var msj = document.createElement("div");
+var cont = document.getElementById("cont");
 
-	var nodoText=document.createTextNode(comments);
+var nodoText=document.createTextNode(comments);
+	
+
 	msj.appendChild(nodoText);
-
 	cont.appendChild(msj);
-}	
+
+}
+
+function desactivar(){
+	
+}
+
+function contar() { 
+         var max = "140"; 
+         var cadena = document.getElementById("msj").value; 
+         var longitud = cadena.length; 
+
+             if(longitud <= max) { 
+                  document.getElementById("contador").value = max-longitud; 
+             } else if (logitud >=120){
+             	contador.style.color = red;
+             } else if (logitud >=130){
+             	contador.style.color = blue;
+             } else { 
+                  document.getElementById("msj").value = cadena.substr(0, max);
+             }
+    }
+   
